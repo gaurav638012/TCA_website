ab399da2b52adf800fdead89cd3eff4046eb783b
gaurav638012 <gaurav638012@gmail.com>
2022-04-02 22:19:03 +0530

    sd

--- TCA/src/app/subscription/subscription.component.ts
+++ TCA/src/app/subscription/subscription.component.ts
@@ -18,7 +18,7 @@ declare var Razorpay:any;
 
 export class SubscriptionComponent implements OnInit {
 
-  base_cost = 6;
+  base_cost = 600;
   event_cost = 310;
   total = this.base_cost;
   submitted = false;
