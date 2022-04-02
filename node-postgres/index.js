ed8d59503c8dfd8742762743779f15099f99ec30
gaurav638012 <gaurav638012@gmail.com>
2022-04-02 17:06:15 +0530

    changes

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -14,6 +14,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://tcaiitb-opp3z.ondigitalocean.app'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
